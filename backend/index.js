a70970d207f58488f8142dda33e7d503b2ef3f1f
yashksaini-coder <ys3853428@gmail.com>
2024-09-08 00:28:35 +0530

    Chore: MongoDB initialized & connected, Created schemas

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const { connectDB } = require('./conn');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port 3000');
+});
+
